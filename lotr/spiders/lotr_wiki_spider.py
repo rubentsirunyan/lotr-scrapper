e85679b7c2423f2a5eb3d63d865470d2d5c07219
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2018-03-25 02:07:11 +0400

    Fixing The issue with 'other names'

--- lotr/spiders/lotr_wiki_spider.py
+++ lotr/spiders/lotr_wiki_spider.py
@@ -14,14 +14,14 @@ class LotrWikiSpider(scrapy.Spider):
 
     def __init__(self):
         self.ss = {
-            'characters':  'div#mw-content-text div#mw-pages table tr td',
+            'characters': 'div#mw-content-text div#mw-pages table tr td',
             'next_page': 'div.wikia-paginator a.paginator-next::attr(href)',
             'info': 'section.pi-item div.pi-item',
             'info_key': 'h3.pi-data-label::text',
             'info_value': 'div.pi-data-value.pi-font *::text',
             'name': 'header#PageHeader h1.page-header__title::text',
-            'see_more': "//h3/span[contains(text(), '{}')]\
-                        /parent::*/following-sibling::ul[1]/li/b/text()"
+            'see_more_b': '//*[@id="{}" or @id="{}"]/parent::*/following-sibling::ul[1]/li/*[1]/text()',
+            'see_more_no_b': '//*[@id="{}" or @id="{}"]/parent::*/following-sibling::ul[1]/li/text()',
         }
 
     def parse(self, response):
@@ -40,7 +40,7 @@ class LotrWikiSpider(scrapy.Spider):
     def parse_character_details(self, response):
         self.i = {}
         for s in response.css(self.ss['info']):
-            if "see more" in s.css(self.ss['info_value']).extract():
+            if "see more" in ''.join(s.css(self.ss['info_value']).extract()):
                 self.i[s.css(self.ss['info_key']).extract_first()]\
                  = self.parse_see_more(response, s.css(self.ss['info_key']).extract_first())
             elif "Titles" in s.css(self.ss['info_key']).extract_first()\
@@ -56,8 +56,17 @@ class LotrWikiSpider(scrapy.Spider):
 
     def parse_see_more(self, response, title):
         self.r = response
-        self.t = title.title()
-        return self.r.xpath(self.ss['see_more'].format(self.t)).extract()
+        self.t1 = title.replace(' ', '_')
+        self.t2 = title.title().replace(' ', '_')
+        # if len(title.split()) > 2:
+        #     self.t1 = '_'.join(self.t1.split())
+        #     self.t2 = '_'.join(self.t2.split())
+        if self.r.xpath(self.ss['see_more_b'].format(self.t1, self.t2)):
+            return self.r.xpath(self.ss['see_more_b'].format(self.t1, self.t2)).extract()
+        else:
+            lst = self.r.xpath(self.ss['see_more_no_b'].format(self.t1, self.t2)).extract()
+            lst = [i.replace("\n", '') for i in lst]
+            return lst
 
     def parse_more_details(self, _list):
         self.expr = re.compile("\\[[0-9]\\]")

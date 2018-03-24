f76290cba59c2c6fe91618dd37e8da7de0647f35
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2018-03-24 18:00:18 +0400

    Prettying things

--- lotr/spiders/lotr_wiki_spider.py
+++ lotr/spiders/lotr_wiki_spider.py
@@ -13,47 +13,51 @@ class LotrWikiSpider(scrapy.Spider):
     ]
 
     def __init__(self):
-        self.sections = {
-            'characters':  'div#mw-content-text div div#mw-pages div.mw-content-ltr table tr td',
-            'next_page': 'div.wikia-paginator ul li a.paginator-next::attr(href)',
+        self.ss = {
+            'characters':  'div#mw-content-text div#mw-pages table tr td',
+            'next_page': 'div.wikia-paginator a.paginator-next::attr(href)',
             'info': 'section.pi-item div.pi-item',
             'info_key': 'h3.pi-data-label::text',
             'info_value': 'div.pi-data-value.pi-font *::text',
-            'name': 'header#PageHeader.page-header h1.page-header__title::text',
-            'see_more': "//h3/span[contains(text(), '{}')]/parent::*/following-sibling::ul[1]/li/b/text()"
+            'name': 'header#PageHeader h1.page-header__title::text',
+            'see_more': "//h3/span[contains(text(), '{}')]\
+                        /parent::*/following-sibling::ul[1]/li/b/text()"
         }
 
     def parse(self, response):
 
-        for td in response.css(self.sections['characters']):
+        for td in response.css(self.ss['characters']):
             for li in td.css('ul li'):
                 url = response.urljoin(li.css('a::attr(href)').extract_first())
                 yield scrapy.Request(url=url,
                                      callback=self.parse_character_details)
 
-        next_page = response.css(self.sections['next_page']).extract_first()
+        next_page = response.css(self.ss['next_page']).extract_first()
         if next_page is not None:
             yield response.follow(next_page,
                                   callback=self.parse)
 
     def parse_character_details(self, response):
-        self.info_dict = {}
-        for section in response.css(self.sections['info']):
-            if "see more" in section.css(self.sections['info_value']).extract():
-                self.info_dict[section.css(self.sections['info_key']).extract_first()] = self.parse_see_more(response, section.css(self.sections['info_key']).extract_first())
-            elif "Titles" in section.css(self.sections['info_key']).extract_first()\
-                 or "Other names" in section.css(self.sections['info_key']).extract_first()\
-                 or "Weapon" in section.css(self.sections['info_key']).extract_first():
-                self.info_dict[section.css(self.sections['info_key']).extract_first()] = self.parse_more_details(section.css(self.sections['info_value']).extract())
+        self.i = {}
+        for s in response.css(self.ss['info']):
+            if "see more" in s.css(self.ss['info_value']).extract():
+                self.i[s.css(self.ss['info_key']).extract_first()]\
+                 = self.parse_see_more(response, s.css(self.ss['info_key']).extract_first())
+            elif "Titles" in s.css(self.ss['info_key']).extract_first()\
+                 or "Other names" in s.css(self.ss['info_key']).extract_first()\
+                 or "Weapon" in s.css(self.ss['info_key']).extract_first():
+                self.i[s.css(self.ss['info_key']).extract_first()]\
+                 = self.parse_more_details(s.css(self.ss['info_value']).extract())
             else:
-                self.info_dict[section.css(self.sections['info_key']).extract_first()] = ''.join(self.parse_more_details(section.css(self.sections['info_value']).extract()))
-        self.info_dict['Name'] = response.css(self.sections['name']).extract_first()
-        yield self.info_dict
+                self.i[s.css(self.ss['info_key']).extract_first()]\
+                 = ''.join(self.parse_more_details(s.css(self.ss['info_value']).extract()))
+        self.i['Name'] = response.css(self.ss['name']).extract_first()
+        yield self.i
 
     def parse_see_more(self, response, title):
-        self.response = response
-        self.title = title
-        return self.response.xpath(self.sections['see_more'].format(self.title.title())).extract()
+        self.r = response
+        self.t = title.title()
+        return self.r.xpath(self.ss['see_more'].format(self.t)).extract()
 
     def parse_more_details(self, _list):
         self.expr = re.compile("\\[[0-9]\\]")
